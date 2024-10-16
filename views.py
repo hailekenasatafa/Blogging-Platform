a2bc39ff20c5a0f997371d255d83d776cfefad70
Haile Kenasa <142696185+hailekenasatafa@users.noreply.github.com>
2024-10-16 12:38:13 -0400

    Update views.py

--- views.py
+++ views.py
@@ -4,3 +4,7 @@ def validate(self, data):
     if not data['title']:
         raise ValidationError("Title is required")
     return data
+from django_filters.rest_framework import DjangoFilterBackend
+class BlogPostViewSet(viewsets.ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'author']
