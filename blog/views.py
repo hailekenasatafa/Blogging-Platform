b6ad06e47d6bd7e248627d6d9ec98d99657f84de
Haile Kenasa <142696185+hailekenasatafa@users.noreply.github.com>
2024-10-16 12:33:40 -0400

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
 
 class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
